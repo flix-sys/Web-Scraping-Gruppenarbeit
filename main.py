b880c97cbb8865ee7f1021ffaf036fc8eb22fee8
flix-sys <f.grinstein@aloberlin.de>
2026-03-13 08:58:25 +0000

    Base html link for book links scraping

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import pandas as pd
 import requests
 from pyquery import PyQuery as pq
 
-doc = pq(url="https://books.toscrape.com/catalogue/category/books_1/page-1.html")
+url = "https://books.toscrape.com/catalogue/category/books_1/page-1.html"
+response = requests.get(url)
+html = response.text
 
-for link in doc("h3>a"):
-    print(link.text, link.attrib["href"])
+doc = pq(html)
+
+books = []
+
+for link in doc("h3>a").items():
+   title = link.attr("title")
+   href = link.attr("href")
+   books.append({"Title": title, "Links": href})
+
+df = pd.DataFrame(books)
+print(df)
 
